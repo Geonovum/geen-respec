24f9105e7faa081e1c2c0f46ff385047783c685f
Geonovum <>
2023-03-20 14:57:29 +0000

    Update from https://github.com/Geonovum/word2publicatie/commit/e088e91a9ef7e9f6521bb0e226c3242192645867

---
+++ js/config.js
@@ -0,0 +1,43 @@
+var respecConfig =
+{
+  subtitle: "Gebruikershandleiding",
+  pubDomain: "geen",
+  specStatus: "GN-BASIS",
+  specType: "AL",
+  license: "cc-by-nd",
+  publishDate: "2023-01-14",
+  authors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  localBiblio: {
+    "3DTILES":
+    {
+      title: "3D Tiles Specification 1.0",
+      href: "http://docs.opengeospatial.org/cs/18-053r2/18-053r2.html",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Patrick Cozzi",
+        "Sean Lilley",
+        "Gabby Getz"
+      ],
+      date: "2019-01-31",
+      status: "Approved for public release"
+    }
+  },
+  shortName: "respec",
+  format: "markdown",
+  edDraftURI: "https://Geonovum.github.io/geen-respec/",
+  publishISODate: "2023-01-14T00:00:00.000Z",
+  generatedSubtitle: "Document 14 januari 2023"
+}
