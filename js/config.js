2a1b7d3e93ad1dfaee106d17c08279b2bb9a4634
GN-BOT-WORKFLOW <gn-bot-workflow@users.noreply.github.com>
2026-04-08 15:20:10 +0200

    automated commit

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.respecConfig = {
+  "generatedBy": "new-beheerportaal",
+  "generatedAt": "2026-04-08T13:20:06.894Z",
+  "repository": "geen-respec",
+  "sourceFile": "Handleiding (1).docx"
+};
