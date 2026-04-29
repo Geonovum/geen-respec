da6a5a46db5ede626a8ba31407b1f4b45df524ba
GN-BOT-WORKFLOW <gn-bot-workflow@users.noreply.github.com>
2026-04-29 06:38:09 +0000

    automated commit

---
+++ js/config.js
@@ -0,0 +1,43 @@
+var documentConfig =
+{
+  title: "Gebruikershandleiding word2respec",
+  shortName: "respec",
+  pubDomain: "geen",
+  specStatus: "basis",
+  specType: "bd",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/geen/respec/"
+  ],
+  publishDate: "2025-11-17",
+  publishVersion: "1.2.0",
+  thisVersion: [
+    "https://docs.geostandaarden.nl/geen/basis-bd-respec-20251117/"
+  ],
+  previousPublishDate: "2025-07-16",
+  previousPublishVersion: "1.1.0",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/geen/geen-bd-respec-20250716/"
+  ],
+  previousMaturity: "geen",
+  edDraftURI: "https://geonovum.github.io/geen-respec/",
+  canonicalURI: "https://docs.geostandaarden.nl/geen/respec/",
+  editors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  localBiblio: {
+    "DONL": {
+      title: "data.overheid.nl",
+      href: "https://data.overheid.nl",
+      authors: "",
+      publisher: "Kennis- en exploitatiecentrum voor Officiële Overheidspublicaties",
+      editors: "",
+      date: "",
+      status: ""
+    }
+  }
+}
